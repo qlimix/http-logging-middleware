b0981a7530888d8241de72ed04029c60ab733546
frank <frank@qlimix.net>
2018-03-13 20:23:54 +0100

    Response & request logging middleware

--- src/RequestLoggerMiddleware.php
+++ src/RequestLoggerMiddleware.php
@@ -6,14 +6,28 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Qlimix\Log\Logger\Request\RequestLoggerInterface;
 
 final class RequestLoggerMiddleware implements MiddlewareInterface
 {
+    /** @var RequestLoggerInterface */
+    private $requestLogger;
+
+    /**
+     * @param RequestLoggerInterface $requestLogger
+     */
+    public function __construct(RequestLoggerInterface $requestLogger)
+    {
+        $this->requestLogger = $requestLogger;
+    }
+
     /**
      * @inheritDoc
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        $this->requestLogger->log($request);
 
+        return $handler->handle($request);
     }
 }
--- src/ResponseLogger.php
+++ src/ResponseLogger.php
@@ -6,14 +6,30 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Qlimix\Log\Logger\Response\ResponseLoggerInterface;
 
 final class ResponseLogger implements MiddlewareInterface
 {
+    /** @var ResponseLoggerInterface */
+    private $responseLogger;
+
+    /**
+     * @param ResponseLoggerInterface $responseLogger
+     */
+    public function __construct(ResponseLoggerInterface $responseLogger)
+    {
+        $this->responseLogger = $responseLogger;
+    }
+
     /**
      * @inheritDoc
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        $response = $handler->handle($request);
 
+        $this->responseLogger->log($response);
+
+        return $response;
     }
 }
